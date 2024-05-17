25ece95efe0b6dd31807abf4c73c6e3ca1872683
YuliaPolakova <165685354+YuliaPolakova@users.noreply.github.com>
2024-05-17 18:31:52 +0300

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -4,5 +4,5 @@ age = 21
 print('age:', age)
 age = age+1
 print('age:' , age)
-Yuliya = 'is_student'
-print('Is student:' , Yuliya == 'is_student')
+Yuliya = is_student
+print('Is student:' , Yuliya == is_student)
