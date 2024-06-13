6c91f44ed3d669e309e2eb52a0132243b464e6a3
YuliaPolakova <165685354+YuliaPolakova@users.noreply.github.com>
2024-06-13 22:10:20 +0300

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,7 @@
+my_strining = 'Приветик, я люблю кофе по утрам'
+print(len(my_strining))
+print(my_strining .upper())
+print(my_strining .lower())
+print(my_strining .replace(__old:' ', __new:''))
+print(my_strining[0])
+print(my_strining[-1])
---
+++ hw5.py
@@ -0,0 +1,9 @@
+immutable_var = ["Tea", 31, True]
+print(immutable_var)
+immutable_var [0] = 43
+print(immutable_var)
+
+mutable_list = [3, 7, "Coffe", "Milk"]
+print(mutable_list)
+mutable_list [1] = 1
+print(mutable_list)
