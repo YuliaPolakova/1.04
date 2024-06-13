494e2cb6a00ca28a4d6f76499c2e3e44b095d2b8
YuliaPolakova <165685354+YuliaPolakova@users.noreply.github.com>
2024-06-13 22:37:12 +0300

    hw4.py

--- hw4.py
+++ hw4.py
@@ -2,6 +2,8 @@ my_strining = 'Приветик, я люблю кофе по утрам'
 print(len(my_strining))
 print(my_strining .upper())
 print(my_strining .lower())
-print(my_strining .replace(__old:' ', __new:''))
+my_strining_no_spase = my_strining .replace( ' ', '')
+print(my_strining_no_spase)
 print(my_strining[0])
 print(my_strining[-1])
+
